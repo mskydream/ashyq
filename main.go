e8fba49901f2a0491fa05c6cd01eef609dcbebef
just.aidyn <aidyn_o_b@mail.ru>
2022-07-05 16:48:08 +0600

    удалил ненужные картинки и было изменение в ERD

--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
 	"github.com/skip2/go-qrcode"
 )
 
-// import fyne
-
 func main() {
 	// first example
 	// err := qrcode.WriteFile("hello this is qrcode in golang", qrcode.Medium, 256, "myfirst_file.png")
@@ -17,7 +15,7 @@ func main() {
 	// }
 
 	// second example
-	err := qrcode.WriteColorFile("this is colored", qrcode.Medium, 256, color.Transparent, color.Black, "exparimant.jpg")
+	err := qrcode.WriteColorFile("this is colored", qrcode.Medium, 256, color.Transparent, color.White, "exparimant.jpg")
 	if err != nil {
 		fmt.Printf("Sorry couldn't create qrcode: %v\n", err)
 	}
