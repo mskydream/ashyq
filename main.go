ce352c6bffb4e567c47f939d41ccfd32ecc17a86
just.aidyn <aidyn_o_b@mail.ru>
2022-07-07 10:05:52 +0600

    added folders

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"image/color"
+	"math"
 
 	"github.com/skip2/go-qrcode"
 )
@@ -15,8 +16,9 @@ func main() {
 	// }
 
 	// second example
-	err := qrcode.WriteColorFile("this is colored", qrcode.Medium, 256, color.Transparent, color.White, "exparimant.jpg")
+	err := qrcode.WriteColorFile("this is colored", qrcode.High, 256, color.Transparent, color.RGBA{0, 0, 255, 1}, "experiment.png")
 	if err != nil {
 		fmt.Printf("Sorry couldn't create qrcode: %v\n", err)
 	}
+	fmt.Println(math.Pow(7089, 10))
 }
